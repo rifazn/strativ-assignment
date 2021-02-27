008117ae938ebf31322024a863f28761e8b76e30
Rifaz Nahiyan <rifazn@protonmail.com>
2021-02-28 04:06:05 +0600

    details of specific country (partially)

--- restcountries/urls.py
+++ restcountries/urls.py
@@ -7,6 +7,11 @@ from . import views
 app_name = 'restcountries'
 urlpatterns = [
     path('', views.index, name='index'),
+
     # Paths for exposing the REST API
-    path('rest/all', views.api_all, name='api_all'),
+
+    # JSON response containing all countries in a list
+    path('rest/all/', views.api_all, name='api_all'),
+    # JSON response of a single country
+    path('rest/country/<str:name>/', views.api_country_name, name='api_country_name'),
 ]
--- restcountries/views.py
+++ restcountries/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
+from django.forms.models import model_to_dict
 
 from .models import Country
 
@@ -9,6 +10,15 @@ def api_all(request):
     countries_list = list(countries.values())
 
     return JsonResponse(countries_list, safe=False)
+
+def api_country_name(request, name):
+    try:
+        country = Country.objects.get(name=name.capitalize())
+        return JsonResponse(model_to_dict(country))
+    except Exception as er:
+        print(er)
+        err = {'message': 'Sorry. That country name does not exist'}
+        return JsonResponse(err, status=404)
 
 def index(request):
     return HttpResponse("Hello!")
