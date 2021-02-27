461a237538644c89f184583b596875cd73cb999e
Rifaz Nahiyan <rifazn@protonmail.com>
2021-02-28 01:00:48 +0600

    Model for countries created

--- restcountries/models.py
+++ restcountries/models.py
@@ -2,4 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Country(models.Model):
-    pass
+    """Country model having all attributes as specified"""
+    name = models.CharField(max_length=200)
+    alphacode2 = models.CharField(max_length=3)
+    capital = models.CharField(max_length=200)
+    population = models.BigIntegerField()
+    timezone = models.CharField(max_length=20)
+    flag = models.CharField(max_length=200)
+    languages = models.CharField(max_length=500) # TODO: make it a choicefield
+    neighbouring_countries = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'countries'
+
+    def __str__(self):
+        return f'{self.name[:50]}'
