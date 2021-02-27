bc434c7266f205cbcfe0f91cce50e9fede9c2a06
Rifaz Nahiyan <rifazn@protonmail.com>
2021-02-28 03:14:50 +0600

    Phase 2 start. The url /rest/all return a JSON response of all countries in database.

--- restcountries/urls.py
+++ restcountries/urls.py
@@ -5,4 +5,8 @@ from django.urls import path
 from . import views
 
 app_name = 'restcountries'
-urlpatterns = []
+urlpatterns = [
+    path('', views.index, name='index'),
+    # Paths for exposing the REST API
+    path('rest/all', views.api_all, name='api_all'),
+]
--- restcountries/views.py
+++ restcountries/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+
+from .models import Country
 
 # Create your views here.
+def api_all(request):
+    countries = Country.objects.all()
+    countries_list = list(countries.values())
+
+    return JsonResponse(countries_list, safe=False)
+
+def index(request):
+    return HttpResponse("Hello!")
