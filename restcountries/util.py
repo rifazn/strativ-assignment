42f8d4c05c4beb13b073b8c09ef6745b72a531bd
Rifaz Nahiyan <rifazn@protonmail.com>
2021-02-28 02:50:14 +0600

    Phase 1 Complete. Database populated by consuming API

--- restcountries/util.py
+++ restcountries/util.py
@@ -1,4 +1,5 @@
 import requests
+from .models import Country
 
 def get_countries():
     """
@@ -22,5 +23,35 @@ def get_countries():
 
     return data
 
-def populate_countries_db():
-    pass
+def populate_countries_db(data):
+    """
+    Initialize the databse by populating with data returned from
+    get_countries().
+    """
+    for row in data:
+        country = Country()
+        country.name = row['name']
+        country.alphacode2 = row['alpha2Code']
+        country.capital = row['capital']
+        country.population = row['population']
+        country.timezone = row['timezones'][0]
+        country.flag = row['flag']
+        country.languages = ', '.join([lang['name'] for lang in row['languages']])
+        country.neighbouring_countries = ', '.join(row['borders'])
+
+        country.save()
+
+def print_countries_db():
+    """
+    Print to stdout all entries from the 'Country' model which is populated by
+    the restcountries API consumption.
+    """
+    args = ['No.', 'name', 'alphacode2', 'capital', 'population', 'timezone', 'flag',
+            'languages', 'neighbouring_countries']
+    print('{:<3}  {:<7}  {:<3}  {:<10}  {:<10}  {:6}  {:<10}  {:<10}  {:<20}'.format(*args))
+
+    countries = Country.objects.all()
+    for country in countries.all().values():
+        print('{:<3}  {:<7}  {:<3}  {:<10}  {:<10}  {:6}  {:<25}  {:<10}  {:<20}'
+              .format(*country.values()))
+
