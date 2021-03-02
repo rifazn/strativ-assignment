ed23959b9da842fd8335b262e0031c56be23e238
Rifaz Nahiyan <rifazn@protonmail.com>
2021-03-03 01:37:19 +0600

    Register new user from the webpage

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    if request.method != "POST":
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            newuser = form.save()
+            login(request, newuser)
+            return redirect('restcountries:index')
+    # Display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
